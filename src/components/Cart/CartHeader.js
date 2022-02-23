a81b44416bb37983ae7666aded631decaade4994
Oleksiy <oleksiymykhalevich86@gmail.com>
2022-02-23 20:20:41 +0200

    add name to cartHeader

--- src/components/Cart/CartHeader.js
+++ src/components/Cart/CartHeader.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { keys } from 'lodash'
+import { productsArray } from 'components/Products/productsArray'
 
 export const CartHeader = ({ productInCart }) => {
     return (
@@ -7,7 +8,8 @@ export const CartHeader = ({ productInCart }) => {
             <div>
                 {keys(productInCart).map((productId) => (
                     <div key={productId}>
-                        {productId} : {productInCart[productId]}
+                        {productsArray[productId - 1].name} :{' '}
+                        {productInCart[productId]}
                     </div>
                 ))}
             </div>
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -7,12 +7,9 @@ import { useState } from 'react'
 import { productsArray } from 'components/Products/productsArray'
 
 export const App = () => {
-    const [productInCart, setProductInCart] = useState({
-        1: 5,
-        2: 3,
-    })
+    const [productInCart, setProductInCart] = useState({})
 
-    const addProductToCart = (id, count) => {
+    const addProductToCart = (id, count, name) => {
         setProductInCart((prevState) => ({
             ...prevState,
             [id]: (prevState[id] || 0) + count,
